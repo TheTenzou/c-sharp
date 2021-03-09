06f036138a29889329ea037f5ca3b233fd601ad2
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-09 16:15:51 +0300

    Lab 6 task2

---
+++ lab6/Task2/Task2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Refresh();
+            if (e.Button == MouseButtons.Right)
+            {
+                Graphics dc = CreateGraphics();
+                dc.DrawLine(Pens.DarkTurquoise, 0, 0, e.X, e.Y);
+            }
+        }
+    }
+}
