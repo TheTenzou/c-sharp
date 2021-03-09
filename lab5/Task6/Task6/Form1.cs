57d676f2740cc2a49baf2bfee3c28420bac2783a
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-09 15:39:44 +0300

    task6

---
+++ lab5/Task6/Task6/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task6
+{
+    public partial class Form1 : Form
+    {
+        int x = 50;
+        int y = 50;
+        int r = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Down) y += 10;
+            if (e.KeyData == Keys.Up) y -= 10;
+            if (e.KeyData == Keys.Left) x -= 10;
+            if (e.KeyData == Keys.Right) x += 10;
+            Refresh();
+            Pen pen = new Pen(Color.Black);
+            Graphics g = CreateGraphics();
+            g.DrawEllipse(pen, x, y, r, r);
+
+        }
+    }
+}
