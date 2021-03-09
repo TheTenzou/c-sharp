a8b07cd46f9995408ef933deeffdc940c58f86fe
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-09 16:09:07 +0300

    task6

---
+++ lab6/Task1/Task1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            String eventString = "";
+            Refresh();
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    eventString = "Left";
+                    break;
+                case MouseButtons.Right:
+                    eventString = "Right";
+                    break;
+                case MouseButtons.Middle:
+                    eventString = "Middle";
+                    break;
+                case MouseButtons.None:
+                default:
+                    break;
+            }
+            Graphics dc = CreateGraphics();
+            Font fnt = new Font("Arial", 20); //Шрифт Arial, размер 10
+            dc.DrawString(eventString, fnt, Brushes.Blue, e.X, e.Y);
+        }
+    }
+}
