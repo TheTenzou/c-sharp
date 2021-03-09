9729becb75967a98f3224787252c1e6ca21473d4
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-09 16:24:58 +0300

    lab6 task3

---
+++ lab6/Task3/Task3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Point last_point;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = CreateGraphics();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point new_point = new Point(e.X, e.Y);
+            graphics.DrawLine(Pens.DarkTurquoise, last_point, new_point);
+            last_point = new_point;
+        }
+    }
+}
