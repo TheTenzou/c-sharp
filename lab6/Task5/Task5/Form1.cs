7d5136ea8a9c2cd4787ac5f386d17e9a169366ce
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-09 21:02:17 +0300

    lab6 task5

---
+++ lab6/Task5/Task5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task5
+{
+    public partial class Form1 : Form
+    {
+        int t = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            int width_rec = 100;
+            Graphics graphics = e.Graphics;
+
+            Rectangle rec_left = new Rectangle(200 + t, 200 + t, width_rec, width_rec);
+            Rectangle rec_right = new Rectangle(200 - t, 200 - t, width_rec, width_rec);
+
+            graphics.DrawRectangle(Pens.RoyalBlue, rec_left);
+            graphics.DrawRectangle(Pens.RoyalBlue, rec_right);
+
+            graphics.DrawRectangle(Pens.GreenYellow, Rectangle.Intersect(rec_left, rec_right));
+
+            graphics.DrawRectangle(Pens.DarkKhaki, Rectangle.Union(rec_left, rec_right));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += 2;
+            Invalidate();
+        }
+    }
+}
