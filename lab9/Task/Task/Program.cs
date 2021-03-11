bcd312db8caad5ebc350d412783d69949192f463
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-11 14:35:38 +0300

    lab9

---
+++ lab9/Task/Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Strings = new string[255];
+            Console.Write("String count: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Strings[i] = Console.ReadLine();
+            }
+            
+            Console.Write("Enter substring: ");
+            string str_find = Console.ReadLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Strings[i].IndexOf(str_find) != -1)
+                {
+                    Console.WriteLine(i + 1 + ": " + Strings[i]);
+                }
+            }
+        }
+    }
+}
