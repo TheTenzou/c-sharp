38e021625689552c23a949f17b74200dec6946b5
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-13 14:32:53 +0300

    init lab10

---
+++ lab10/Task1/Task1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.tabTable = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.buttonCeateTable = new System.Windows.Forms.Button();
+            this.TabControl.SuspendLayout();
+            this.tabTable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.tabTable);
+            this.TabControl.Controls.Add(this.tabPage2);
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(600, 400);
+            this.TabControl.TabIndex = 0;
+            // 
+            // tabTable
+            // 
+            this.tabTable.Controls.Add(this.dataGridView1);
+            this.tabTable.Location = new System.Drawing.Point(4, 25);
+            this.tabTable.Name = "tabTable";
+            this.tabTable.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTable.Size = new System.Drawing.Size(592, 371);
+            this.tabTable.TabIndex = 0;
+            this.tabTable.Text = "Table";
+            this.tabTable.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(535, 245);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(592, 371);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // buttonCeateTable
+            // 
+            this.buttonCeateTable.Location = new System.Drawing.Point(622, 38);
+            this.buttonCeateTable.Name = "buttonCeateTable";
+            this.buttonCeateTable.Size = new System.Drawing.Size(123, 23);
+            this.buttonCeateTable.TabIndex = 0;
+            this.buttonCeateTable.Text = "Create table";
+            this.buttonCeateTable.UseVisualStyleBackColor = true;
+            this.buttonCeateTable.Click += new System.EventHandler(this.buttonCeateTable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCeateTable);
+            this.Controls.Add(this.TabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TabControl.ResumeLayout(false);
+            this.tabTable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage tabTable;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonCeateTable;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ lab10/Task1/Task1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCeateTable_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            DataTable Table1 = new DataTable();
+            DataColumn column;
+            DataRow row;
+            column = new DataColumn();
+            column.ColumnName = "Teacher_id";
+            column.DataType = typeof(int);
+            column.AutoIncrement = true;
+            Table1.Columns.Add(column);
+
+            Table1.TableName = "Teachers";
+            column = new DataColumn();
+
+            column.ColumnName = "Name";
+            column.DataType = typeof(string);
+            Table1.Columns.Add(column);
+
+
+            row = Table1.NewRow();
+            row["Name"] = "First teacher";
+            Table1.Rows.Add(row);
+
+            row = Table1.NewRow();
+            row["Name"] = "Second teacher";
+            Table1.Rows.Add(row);
+
+            row = Table1.NewRow();
+            row["Name"] = "Third teacher";
+            Table1.Rows.Add(row);
+
+            dataGridView1.DataSource = Table1;
+            dataGridView1.Columns[1].Width = 200;
+/*
+            dataGridView2.DataSource = ds;
+            dataGridView2.DataMember = "Order";
+
+            dataGridView3.DataSource = ds;
+            dataGridView3.DataMember = "OrderLines";
+*/
+        }
+    }
+}
